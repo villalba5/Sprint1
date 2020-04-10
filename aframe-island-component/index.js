8354d9662e08a78acc6dae78064fe0617698fc09
Álvaro Vilalba <alvaro.villalba@talentomobile.com>
2020-04-10 17:42:49 +0200

    github corner fixed

--- aframe-island-component/index.js
+++ aframe-island-component/index.js
@@ -60,44 +60,6 @@ AFRAME.registerComponent('newisland', {
 		//console.log(this.data);
 		//console.log("Pinto una caja",data.posx, ',', data.posz);
 	},
-
-	/**
-	 * Called when component is attached and when component data changes.
-	 * Generally modifies the entity based on the data.
-	 */
-	update: function (oldData) {
-
-	},
-
-	/**
-	 * Called when a component is removed (e.g., via removeAttribute).
-	 * Generally undoes all modifications to the entity.
-	 */
-	remove: function () { },
-
-	/**
-	 * Called on each scene tick.
-	 */
-	// tick: function (t) { },
-
-	/**
-	 * Called when entity pauses.
-	 * Use to stop or remove any dynamic or background behavior such as events.
-	 */
-	pause: function () { },
-
-	/**
-	 * Called when entity resumes.
-	 * Use to continue or add any dynamic or background behavior such as events.
-	 */
-	play: function () { },
-
-	/**
-	 * Event handlers that automatically get attached or detached based on scene state.
-	 */
-	events: {
-		// click: function (evt) { }
-	}
 });
 
 
@@ -160,59 +122,6 @@ AFRAME.registerComponent('islands', {
 		}
 	},
 });
-
-
-// AFRAME.registerComponent('concentricislands', {
-
-// 	schema: {
-// 		height: { type: 'number', default: 1 },
-// 		area: { type: 'number', default: 1 },
-// 		databox: { type: 'asset' },
-// 		geometry: { type: 'string', default: 'box' }
-// 	},
-// 	init: function () {
-// 		//Load de json file
-// 		this.loader = new THREE.FileLoader();
-// 		let data = this.data;
-// 		if (data.databox) {
-// 			this.loader.load(data.databox, this.onDataLoaded.bind(this));
-// 		}
-// 	},
-
-
-
-// 	onDataLoaded: function (file) { //in this function i parse the json file and get the objects that i will represent
-
-// 		let rightside = []
-// 		let leftside = []
-// 		let topside = []
-// 		let bottomside = []
-
-
-// 		//console.log('entrando onDataLoaded');
-
-// 		// create box for each json objet
-
-// 		let data = this.data;
-// 		console.log(data);
-
-// 		//console.log(data);
-// 		let elements = JSON.parse(file);
-// 		//console.log(elements);
-
-// 		let scene = document.querySelector('a-scene');
-
-// 		printFirst(elements[0], scene, rightside, leftside, topside, bottomside); //print the first element in the center of the scene
-
-// 		//console.log(rightside);
-
-// 		printCircles(elements, scene, rightside, leftside, topside, bottomside);
-// 	},
-// });
-
-
-
-
 
 //-----FUNCTIONS-----
 
@@ -325,187 +234,6 @@ function printBoxes(boxes, positioning, num, elem) {
 			break;
 	}
 }
-
-// function printCylinders(cylinders, positioning) {
-// 	console.log(cylinders, positioning);
-// 	switch (positioning) {
-// 		case 'random':
-// 			//console.log('es raaaandom');
-// 			this.randomPositionsCylinders(cylinders);
-// 			break;
-// 		case 'four':
-// 			//console.log('es four');
-// 			this.fourincircleCylinders(cylinders);
-// 			break;
-
-// 		default:
-// 			break;
-// 	}
-// }
-
-// function randomPositionsCylinders(cylinders) {
-
-// 	let scene = document.querySelector('a-scene');
-
-// 	cylinders.forEach(function (cylinder) {
-
-// 		let posx = Math.floor(Math.random() * 11) * Math.pow(-1, Math.floor(Math.random() * 2))
-// 		let posz = Math.floor(Math.random() * 11) * Math.pow(-1, Math.floor(Math.random() * 2))
-
-// 		//console.log('<<<<<<<<< pintando nuevo cylinder >>>>>>>>>>');
-// 		//console.log(cylinder);
-
-// 		let entity = document.createElement('a-entity');
-
-// 		entity.setAttribute('newisland', {
-// 			'color': 'blue',
-// 			'depth': cylinder.depth,
-// 			'height': cylinder.height,
-// 			'width': cylinder.width,
-// 			'posx': posx,
-// 			'posz': posz,
-// 			'geometry': 'cylinder'
-// 		});
-// 		scene.appendChild(entity);
-// 	});
-// }
-
-
-// function BoxesFourInCircle(boxes) {
-// 	//in this function i'm going to place the first box in the center and the next in concentric circles, 4 for circle
-
-// 	let scene = document.querySelector('a-scene');
-// 	const margin = 0.1
-
-// 	for (let index = 0; index < boxes.length; index++) {
-// 		const box = boxes[index];
-// 		numcircles = Math.ceil(boxes.length / 4)
-// 		//console.log('Numero de boxes : ' + boxes.length + ' Numero de circulos : ' + numcircles);
-
-// 		switch (index) { //the first box in the middle
-// 			case 0:
-// 				posx = 0
-// 				posz = 0
-// 				break;
-// 			case 1:
-// 				posx = 0
-// 				posz = boxes[0].depth / 2 + margin + boxes[index].depth / 2
-// 				break
-// 			case 2:
-// 				posz = 0
-// 				posx = -(boxes[0].width / 2) - margin - boxes[index].width / 2
-// 				break
-// 			case 3:
-// 				posx = 0
-// 				posz = -(boxes[0].depth / 2) - margin - boxes[index].depth / 2
-// 				break
-// 			case 4:
-// 				posz = 0
-// 				posx = (boxes[0].width / 2) + margin + boxes[index].width / 2
-// 				break
-// 			default:
-// 				console.log('error en el switch de los index four in circle');
-// 				break;
-// 		}
-// 		let entity = document.createElement('a-entity');
-
-// 		entity.emit('physicscollided', false);
-
-// 		entity.setAttribute('newisland', {
-// 			'depth': box.depth,
-// 			'height': box.height,
-// 			'width': box.width,
-// 			'posx': posx,
-// 			'posz': posz
-// 		});
-// 		scene.appendChild(entity);
-// 	}
-
-// 	entity.addEventListener('physicscollided', function (event) {
-// 		console.log('Entity collided with', event.detail.collidingEntity);
-// 		//Ahora deberemos manejar la posición para que no choque con ninguno
-// 	});
-// }
-
-// function BoxesRandomPositions(boxes) {
-
-// 	let scene = document.querySelector('a-scene');
-
-// 	boxes.forEach(function (box) {
-
-// 		let posx = Math.floor(Math.random() * 11) * Math.pow(-1, Math.floor(Math.random() * 2))
-// 		let posz = Math.floor(Math.random() * 11) * Math.pow(-1, Math.floor(Math.random() * 2))
-
-// 		//console.log('<<<<<<<<< pintando nuevo box >>>>>>>>>>');
-// 		//console.log(box);
-
-// 		let entity = document.createElement('a-entity');
-
-// 		entity.setAttribute('newisland', {
-// 			'color': 'blue',
-// 			'depth': box.depth,
-// 			'height': box.height,
-// 			'width': box.width,
-// 			'posx': posx,
-// 			'posz': posz
-// 		});
-// 		scene.appendChild(entity);
-// 	});
-
-// }
-
-// function fourincircleCylinders(cylinders) {
-// 	//console.log(cylinders);
-// 	let scene = document.querySelector('a-scene');
-// 	const margin = 0.1
-// 	numcircles = Math.ceil(cylinders.length / 4)
-// 	//console.log('Numero de cylinders : ' + cylinders.length + ' Numero de circulos : ' + numcircles);
-
-// 	radius = []
-
-// 	for (let index = 0; index < cylinders.length; index++) {
-// 		const cylinder = cylinders[index];
-// 		radius.push(Math.sqrt((cylinders[index].width * cylinders[index].height) / 3.1415)); //Array os radius
-
-// 		switch (index) { //the first cylinder in the middle
-// 			case 0:
-// 				posx = 0
-// 				posz = 0
-// 				break;
-// 			case 1:
-// 				posx = 0
-// 				posz = radius[0] + margin + radius[index]
-// 				break
-// 			case 2:
-// 				posz = 0
-// 				posx = -(radius[0]) - margin - radius[index]
-// 				break
-// 			case 3:
-// 				posx = 0
-// 				posz = -(radius[0]) - margin - radius[index]
-// 				break
-// 			case 4:
-// 				posz = 0
-// 				posx = radius[0] + margin + radius[index]
-// 				break
-// 			default:
-// 				console.log('error en el switch de los index four in circle');
-// 				break;
-// 		}
-// 		let entity = document.createElement('a-entity');
-
-// 		entity.setAttribute('newisland', {
-// 			'depth': cylinder.depth,
-// 			'height': cylinder.height,
-// 			'width': cylinder.width,
-// 			'posx': posx,
-// 			'posz': posz,
-// 			'geometry': 'cylinder'
-// 		});
-// 		scene.appendChild(entity);
-// 	}
-// }
-
 
 
 function BoxesConcentric(boxes, num, elem) {
@@ -981,6 +709,13 @@ function BoxesConcentric(boxes, num, elem) {
 					}
 					next++
 
+
+
+
+
+
+					
+
 				} else if (searchtop) {
 						xx = right[0].x
 						zz = right[0].z
@@ -994,7 +729,6 @@ function BoxesConcentric(boxes, num, elem) {
 
 						console.log('old : ', top[next]);
 
-						console.log('x :',xx, 'z :',zz,'len :',leen);
 
 						//replace the old object with the new one
 						top[next].x = posx
